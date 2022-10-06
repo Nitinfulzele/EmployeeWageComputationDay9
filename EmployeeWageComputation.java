f576caa2a8a71414d62a45eb7756a2a492af2d6d
Nitinfulzele <nitinfulzele99@gmail.com>
2022-10-06 21:42:59 +0530

    Attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,11 +1,25 @@
 package EmployeeWageComputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to Employee Wage Computation");
-        System.out.println();
+		System.out.println();
+
+		// Employee attendance
+
+		Random inp = new Random();
+		int empPresent = inp.nextInt(2);
+
+		if (empPresent == 0) {
+			System.out.println("Employee is Present");
+
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
