f84a518889fb14cec728d80ea4c08217a64c69b1
Nitinfulzele <nitinfulzele99@gmail.com>
2022-10-06 21:35:55 +0530

    Employee Wage Computation

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,11 @@
+package EmployeeWageComputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation");
+        System.out.println();
+	}
+
+}
